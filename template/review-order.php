8321dfeb7e75356f6dcc9459aba0b90d8dc659b0
levansy <levansy@gmail.com>
2014-04-01 17:01:36 +0700

    changes order of login form ( Fixes #19)

--- template/review-order.php
+++ template/review-order.php
@@ -5,6 +5,7 @@
 
 global $woocommerce;
 $checked = get_option('woocommerce_enable_guest_checkout');
+$show_login = (!is_user_logged_in() && $checked==="no" && isset($_REQUEST['pp_action']));
 ?>
 <style type="text/css">
     #payment{
@@ -12,31 +13,73 @@ $checked = get_option('woocommerce_enable_guest_checkout');
     }
 </style>
 <div id="paypalexpress_order_review">
-    <?php
+    <form class="checkout" method="POST" action="<?php echo add_query_arg( 'pp_action', 'payaction', add_query_arg( 'wc-api', 'WC_Gateway_PayPal_Express_AngellEYE', home_url( '/' ) ) );?>">
+        <?php
+            woocommerce_order_review();
+            if ($show_login)
+                echo '</form><!--close the checkout form-->';
+        ?>
+</div>
 
-    if ( !is_user_logged_in() && $checked==="no" && isset($_REQUEST['pp_action']) ):  ?>
-        <form class="checkout" method="POST" action="">
-            <?php woocommerce_order_review();?>
-        </form>
-        <style type="text/css">
 
-            .woocommerce #content p.form-row input.button,
-            .woocommerce #respond p.form-row input#submit,
-            .woocommerce p.form-row a.button,
-            .woocommerce p.form-row button.button,
-            .woocommerce p.form-row input.button,
-            .woocommerce-page p.form-row #content input.button,
-            .woocommerce-page p.form-row #respond input#submit,
-            .woocommerce-page p.form-row a.button,
-            .woocommerce-page p.form-row button.button,
-            .woocommerce-page p.form-row input.button{
-                display: block !important;
-            }
-        </style>
-        <header>
-            <h2><?php _e( 'Login', 'woocommerce' ); ?></h2>
-        </header>
-        <form name="" action="" method="post">
+<?php if ( WC()->cart->needs_shipping()  ) : ?>
+
+
+    <header>
+        <h2><?php _e( 'Customer details', 'woocommerce' ); ?></h2>
+    </header>
+
+    <div class="col2-set addresses">
+
+        <div class="col-1">
+
+            <header class="title">
+                <h3><?php _e( 'Shipping Address', 'woocommerce' ); ?></h3>
+            </header>
+            <address><p>
+                    <?php
+                    // Formatted Addresses
+                    $address = array(
+                        'first_name' 	=> WC()->customer->shiptoname,
+                        'last_name'		=> "",
+                        'company'		=> "",
+                        'address_1'		=> WC()->customer->get_address(),
+                        'address_2'		=> "",
+                        'city'			=> WC()->customer->get_city(),
+                        'state'			=> WC()->customer->get_state(),
+                        'postcode'		=> WC()->customer->get_postcode(),
+                        'country'		=> WC()->customer->get_country()
+                    ) ;
+
+                    echo WC()->countries->get_formatted_address( $address );
+                    ?>
+                </p></address>
+
+        </div><!-- /.col-1 -->
+        <div class="col-2">
+        </div><!-- /.col-2 -->
+    </div><!-- /.col2-set -->
+<?php endif; ?>
+<?php if ( $show_login ):  ?>
+    <style type="text/css">
+
+        .woocommerce #content p.form-row input.button,
+        .woocommerce #respond p.form-row input#submit,
+        .woocommerce p.form-row a.button,
+        .woocommerce p.form-row button.button,
+        .woocommerce p.form-row input.button,
+        .woocommerce-page p.form-row #content input.button,
+        .woocommerce-page p.form-row #respond input#submit,
+        .woocommerce-page p.form-row a.button,
+        .woocommerce-page p.form-row button.button,
+        .woocommerce-page p.form-row input.button{
+            display: block !important;
+        }
+    </style>
+    <header>
+        <h2><?php _e( 'Login', 'woocommerce' ); ?></h2>
+    </header>
+    <form name="" action="" method="post">
         <?php
         function curPageURL() {
             $pageURL = 'http';
@@ -60,80 +103,37 @@ $checked = get_option('woocommerce_enable_guest_checkout');
         $result = unserialize(WC()->session->RESULT);
         $email = (!empty($_POST['email']))?$_POST['email']:$result['EMAIL'];
         ?>
-        </form>
-        <header>
-            <h2><?php _e( 'Create A New Account', 'woocommerce' ); ?></h2>
-        </header>
-        <form action="" method="post">
-            <p class="form-row form-row-first">
-                <label for="paypalexpress_order_review_email">Email:<span class="required">*</span></label>
-                <input style="width: 100%;" type="email" name="email" id="paypalexpress_order_review_email" value="<?php echo $email; ?>" />
-            </p>
-            <div class="clear"></div>
-            <p class="form-row form-row-first">
-                <label for="paypalexpress_order_review_password">Password:<span class="required">*</span></label>
-                <input type="password" name="password" id="paypalexpress_order_review_password" class="input-text" />
-            </p>
-            <p class="form-row form-row-last">
-                <label for="paypalexpress_order_review_repassword">Re Password:<span class="required">*</span></label>
-                <input type="password" name="repassword" id="paypalexpress_order_review_repassword" class="input-text"/>
-            </p>
-            <div class="clear"></div>
-            <p>
-                <input class="button" type="submit" name="createaccount" value="Create Account" />
-                <input type="hidden" name="address" value="<?php echo WC()->customer->get_address(); ?>">
-            </p>
-        </form>
-
-    <?php else: ?>
-        <?php
-        echo '<form class="checkout" method="POST" action="' . add_query_arg( 'pp_action', 'payaction', add_query_arg( 'wc-api', 'WC_Gateway_PayPal_Express_AngellEYE', home_url( '/' ) ) ) . '">';
-        woocommerce_order_review();
+    </form>
+    <header>
+        <h2><?php _e( 'Create A New Account', 'woocommerce' ); ?></h2>
+    </header>
+    <form action="" method="post">
+        <p class="form-row form-row-first">
+            <label for="paypalexpress_order_review_email">Email:<span class="required">*</span></label>
+            <input style="width: 100%;" type="email" name="email" id="paypalexpress_order_review_email" value="<?php echo $email; ?>" />
+        </p>
+        <div class="clear"></div>
+        <p class="form-row form-row-first">
+            <label for="paypalexpress_order_review_password">Password:<span class="required">*</span></label>
+            <input type="password" name="password" id="paypalexpress_order_review_password" class="input-text" />
+        </p>
+        <p class="form-row form-row-last">
+            <label for="paypalexpress_order_review_repassword">Re Password:<span class="required">*</span></label>
+            <input type="password" name="repassword" id="paypalexpress_order_review_repassword" class="input-text"/>
+        </p>
+        <div class="clear"></div>
+        <p>
+            <input class="button" type="submit" name="createaccount" value="Create Account" />
+            <input type="hidden" name="address" value="<?php echo WC()->customer->get_address(); ?>">
+        </p>
+    </form>
+<?php else:
+    if ( (is_user_logged_in() || $checked==="yes") && isset($_REQUEST['pp_action']) ){
+        echo '<div class="clear"></div>';
         echo '<p><a class="button cancel" href="' . $woocommerce->cart->get_cart_url() . '">'.__('Cancel order', 'paypal-for-woocommerce').'</a> ';
         echo '<input type="submit" class="button" value="' . __( 'Place Order','paypal-for-woocommerce') . '" /></p>';
-        echo '</form>';
-        ?>
-    <?php endif; ?>
-</div>
-
-
-<?php if ( WC()->cart->needs_shipping()  ) : ?>
-
-
-<header>
-    <h2><?php _e( 'Customer details', 'woocommerce' ); ?></h2>
-</header>
-
-<div class="col2-set addresses">
-
-    <div class="col-1">
-
-        <header class="title">
-            <h3><?php _e( 'Shipping Address', 'woocommerce' ); ?></h3>
-        </header>
-        <address><p>
-                <?php
-                // Formatted Addresses
-                $address = array(
-                    'first_name' 	=> WC()->customer->shiptoname,
-                    'last_name'		=> "",
-                    'company'		=> "",
-                    'address_1'		=> WC()->customer->get_address(),
-                    'address_2'		=> "",
-                    'city'			=> WC()->customer->get_city(),
-                    'state'			=> WC()->customer->get_state(),
-                    'postcode'		=> WC()->customer->get_postcode(),
-                    'country'		=> WC()->customer->get_country()
-                ) ;
-
-                echo WC()->countries->get_formatted_address( $address );
-                ?>
-            </p></address>
-
-    </div><!-- /.col-1 -->
-    <div class="col-2">
-    </div><!-- /.col-2 -->
-</div><!-- /.col2-set -->
+    }
+    ?>
+    </form><!--close the checkout form-->
 <?php endif; ?>
-
 <div class="clear"></div>
