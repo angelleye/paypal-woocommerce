c901d85030a1833dbf36813e301e3c970dfca485
nishitlangaliya <nishit.langaliya@multidots.in>
2015-07-27 19:18:39 +0530

    Shipping information from paypal not saved #253

--- classes/wc-gateway-paypal-express-angelleye.php
+++ classes/wc-gateway-paypal-express-angelleye.php
@@ -1064,6 +1064,9 @@ class WC_Gateway_PayPal_Express_AngellEYE extends WC_Payment_Gateway {
                 }
 
                 $checkout_form_data = maybe_unserialize($this->get_session('checkout_form'));
+                if (isset($this->billing_address) && $this->billing_address =='yes' || empty($checkout_form_data['billing_country'])) {
+                	$checkout_form_data = array();
+                }
                 if (isset($checkout_form_data) && !empty($checkout_form_data)) {
                     foreach ($checkout_form_data as $key => $value) {
                         if (strpos($key, 'billing_') !== false && !empty($value) && !is_array($value)) {
