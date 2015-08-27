b91b7fe712ab119ea1e78b2d1d5c0977fc4563f3
Andrew Angell <service@angelleye.com>
2015-08-27 00:01:37 -0500

    Resolves #290

--- classes/wc-gateway-paypal-express-angelleye.php
+++ classes/wc-gateway-paypal-express-angelleye.php
@@ -99,9 +99,9 @@ class WC_Gateway_PayPal_Express_AngellEYE extends WC_Payment_Gateway {
      */
     public function get_icon() {
 
-        $image_path = WP_PLUGIN_URL . "/" . plugin_basename(dirname(dirname(__FILE__))) . '/assets/images/paypal.png';
+        $image_path = plugins_url() . "/" . plugin_basename(dirname(dirname(__FILE__))) . '/assets/images/paypal.png';
         if ($this->show_paypal_credit == 'yes') {
-            $image_path = WP_PLUGIN_URL . "/" . plugin_basename(dirname(dirname(__FILE__))) . '/assets/images/paypal-credit.png';
+            $image_path = plugins_url() . "/" . plugin_basename(dirname(dirname(__FILE__))) . '/assets/images/paypal-credit.png';
         }
         $icon = "<img src=\"$image_path\" alt='" . __('Pay with PayPal', 'paypal-for-woocommerce') . "'/>";
         return apply_filters('woocommerce_gateway_icon', $icon, $this->id);
