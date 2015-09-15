c9dbbc928d7efc0d717b47799cd6dc5fe22d226f
Sy <levansy@gmail.com>
2015-09-15 13:38:41 +0700

    fix NameParser class #188

--- classes/lib/NameParser.php
+++ classes/lib/NameParser.php
@@ -18,7 +18,7 @@ class FullNameParser
         $unfiltered_name_parts = explode(" ",$full_name);
         // completely ignore any words in parentheses
         foreach ($unfiltered_name_parts as $word) {
-            if ($word{0} != "(")
+            if (@$word[0] != "(")
                 $name_parts[] = $word;
         }
         $num_words = sizeof($name_parts);
