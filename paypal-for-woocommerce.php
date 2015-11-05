4690dc710e48fb6c23679e65fdf86ba7d4f128bd
Sy <levansy@gmail.com>
2015-11-05 16:49:06 +0700

    fixes #349

--- paypal-for-woocommerce.php
+++ paypal-for-woocommerce.php
@@ -959,6 +959,18 @@ if(!class_exists('AngellEYE_Gateway_Paypal')){
                 return true;
             return false;
         }
+
+        static public function curPageURL() {
+            $pageURL = 'http';
+            if (@$_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+            $pageURL .= "://";
+            if ($_SERVER["SERVER_PORT"] != "80") {
+                $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+            } else {
+                $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+            }
+            return $pageURL;
+        }
     }
 }
 new AngellEYE_Gateway_Paypal();
