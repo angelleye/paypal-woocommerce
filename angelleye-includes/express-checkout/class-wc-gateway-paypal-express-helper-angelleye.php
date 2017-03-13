73442b21e632cf3b7570ebbb47a41213445dd396
Kcppdevelopers <kcppdevelopers@gmail.com>
2017-03-13 19:58:12 +0530

    Express Checkout - Option to add additional item (or not) when clicking EC button, ref #355

--- angelleye-includes/express-checkout/class-wc-gateway-paypal-express-helper-angelleye.php
+++ angelleye-includes/express-checkout/class-wc-gateway-paypal-express-helper-angelleye.php
@@ -25,6 +25,7 @@ class Angelleye_PayPal_Express_Checkout_Helper {
             $this->show_on_checkout = !empty($this->setting['show_on_checkout']) ? $this->setting['show_on_checkout'] : 'top';
             $this->button_position = !empty($this->setting['button_position']) ? $this->setting['button_position'] : 'bottom';
             $this->show_on_cart = !empty($this->setting['show_on_cart']) ? $this->setting['show_on_cart'] : 'yes';
+            $this->prevent_to_add_additional_item = 'yes' === !empty($this->setting['prevent_to_add_additional_item']) ? $this->setting['prevent_to_add_additional_item'] : 'no';
             $this->testmode = 'yes' === !empty($this->setting['testmode']) ? $this->setting['testmode'] : 'yes';
             if( $this->testmode == false ) {
                 $this->testmode = AngellEYE_Utility::angelleye_paypal_for_woocommerce_is_set_sandbox_product();
@@ -488,11 +489,8 @@ class Angelleye_PayPal_Express_Checkout_Helper {
     
     public function angelleye_woocommerce_is_sold_individually($return, $data) {
         if (isset($_REQUEST['express_checkout']) || isset($_REQUEST['express_checkout_x'])) {
-            foreach( WC()->cart->get_cart() as $cart_item_key => $values ) {
-		$_product = $values['data'];
-		if( $data->id == $_product->id ) {
-			return true;
-		}
+            if($this->prevent_to_add_additional_item) {
+		return true;
             }
         }
         return $return;
