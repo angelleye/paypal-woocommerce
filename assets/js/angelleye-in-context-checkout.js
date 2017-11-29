9ad3778f780b95cf196784956a1c0aade36b4bc2
kcppdevelopers <kcppdevelopers@gmail.com>
2017-11-29 15:18:37 +0530

    In-context not working, ref #938

--- assets/js/angelleye-in-context-checkout.js
+++ assets/js/angelleye-in-context-checkout.js
@@ -1,51 +1,48 @@
 ;
 (function ($, window, document) {
     if (angelleye_in_content_param.is_product == 'no') {
-                var target_url = '';
-                window.paypalCheckoutReady = function () {
-               
-                    setInterval(function () {
-                        $('.woocommerce').unblock();
-                    }, 3000);
-                    [ '.paypal_checkout_button', '.paypal_checkout_button_cc_top', '.paypal_checkout_button_cc_bottom', '.paypal_checkout_button_top', '.paypal_checkout_button_bottom', '.paypal_checkout_button_cc'].forEach(function(selector) {
-                        paypal.checkout.setup(
-                            angelleye_in_content_param.payer_id,
-                            {
-                                environment: angelleye_in_content_param.environment,
-                                button: selector,
-                                locale: angelleye_in_content_param.locale,
-                                click: function (event) {
-                                     event.preventDefault();
-                                     paypal.checkout.initXO();
-                                     target_url = $(event.target).parent().attr("href");
-                                     if( $("#wc-paypal_express-new-payment-method").is(':checked') ) {
-                                         target_url = target_url + "&ec_save_to_account=true";
-                                     }
-                                     paypal.checkout.startFlow(target_url);
+        var target_url = '';
+        window.paypalCheckoutReady = function () {
+            setInterval(function () {
+                $('.woocommerce').unblock();
+            }, 3000);
+            ['.paypal_checkout_button', '.paypal_checkout_button_cc_top', '.paypal_checkout_button_cc_bottom', '.paypal_checkout_button_top', '.paypal_checkout_button_bottom', '.paypal_checkout_button_cc'].forEach(function (selector) {
+                paypal.checkout.setup(
+                        angelleye_in_content_param.payer_id,
+                        {
+                            environment: angelleye_in_content_param.environment,
+                            button: selector,
+                            locale: angelleye_in_content_param.locale,
+                            click: function (event) {
+                                event.preventDefault();
+                                paypal.checkout.initXO();
+                                target_url = $(event.target).parent().attr("href");
+                                if (typeof target_url === 'undefined' || target_url === null) {
+                                    target_url = $(event.target).attr("href");
                                 }
+                                if ($("#wc-paypal_express-new-payment-method").is(':checked')) {
+                                    target_url = target_url + "&ec_save_to_account=true";
+                                }
+                                paypal.checkout.startFlow(target_url);
                             }
-                        );
-                     });
-                }
-           
-        
-        
-        
+                        }
+                );
+            });
+        };
     } else {
-       
         window.paypalCheckoutReady = function () {
             paypal.checkout.setup(angelleye_in_content_param.payer_id, {
                 environment: angelleye_in_content_param.environment,
                 click: function (event) {
-                event.preventDefault();
-                paypal.checkout.initXO();
-                    $( '.cart' ).block({
-                            message: null,
-                            overlayCSS: {
+                    event.preventDefault();
+                    paypal.checkout.initXO();
+                    $('.cart').block({
+                        message: null,
+                        overlayCSS: {
                             background: '#fff',
                             opacity: 0.6
-                            }
-	            });
+                        }
+                    });
                     var get_attributes = function () {
                         var select = $('.variations_form').find('.variations select'),
                                 data = {},
@@ -69,15 +66,13 @@
                             'data': data
                         };
                     };
-
                     var data = {
-                            'nonce':      angelleye_in_content_param.generate_cart_nonce,
-                            'qty':        $( '.quantity .qty' ).val(),
-                            'attributes': $( '.variations_form' ).length ? get_attributes().data : [],
-                            'wc-paypal_express-new-payment-method' : $("#wc-paypal_express-new-payment-method").is(':checked'),
-                            'is_cc': $(event.target).hasClass('ec_checkout_page_button_type_pc')
+                        'nonce': angelleye_in_content_param.generate_cart_nonce,
+                        'qty': $('.quantity .qty').val(),
+                        'attributes': $('.variations_form').length ? get_attributes().data : [],
+                        'wc-paypal_express-new-payment-method': $("#wc-paypal_express-new-payment-method").is(':checked'),
+                        'is_cc': $(event.target).hasClass('ec_checkout_page_button_type_pc')
                     };
-
                     $.ajax({
                         type: 'POST',
                         data: data,
@@ -95,17 +90,13 @@
                 },
                 button: ['.paypal_checkout_button', '.paypal_checkout_button_cc'],
                 condition: function () {
-                    if( $('.paypal_checkout_button').hasClass( "disabled" ) ) {
+                    if ($('.paypal_checkout_button').hasClass("disabled")) {
                         return false;
                     } else {
                         return true;
-                    } 
-                  }
+                    }
+                }
             });
-        }
-        
-        
-        
-        
+        };
     }
 })(jQuery, window, document);
