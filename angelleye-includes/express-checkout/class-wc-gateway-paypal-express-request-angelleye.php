cbd8622d0ce64e83d5b74e1ac3b6439268946bfe
angelleye <andrew@angelleye.com>
2018-06-18 14:26:28 -0500

    Updates label, ref PFW-74

--- angelleye-includes/express-checkout/class-wc-gateway-paypal-express-request-angelleye.php
+++ angelleye-includes/express-checkout/class-wc-gateway-paypal-express-request-angelleye.php
@@ -362,9 +362,9 @@ class WC_Gateway_PayPal_Express_Request_AngellEYE {
                     update_post_meta($order->get_id(), 'PayPal Transaction Fee', isset($this->paypal_response['PAYMENTINFO_0_FEEAMT']) ? $this->paypal_response['PAYMENTINFO_0_FEEAMT'] : '');
                 }
                 if ($old_wc) {
-                    update_post_meta($order_id, 'Payment type', isset($this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE']) ? $this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE'] : '');
+                    update_post_meta($order_id, 'Payment Type', isset($this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE']) ? $this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE'] : '');
                 } else {
-                    update_post_meta($order->get_id(), 'Payment type', isset($this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE']) ? $this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE'] : '');
+                    update_post_meta($order->get_id(), 'Payment Type', isset($this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE']) ? $this->paypal_response['PAYMENTINFO_0_PAYMENTTYPE'] : '');
                 }
                 $order->add_order_note(sprintf(__('%s payment Transaction ID: %s', 'paypal-for-woocommerce'), $this->gateway->title, isset($this->paypal_response['PAYMENTINFO_0_TRANSACTIONID']) ? $this->paypal_response['PAYMENTINFO_0_TRANSACTIONID'] : ''));
                 WC()->cart->empty_cart();
