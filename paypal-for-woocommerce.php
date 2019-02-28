4b0b30ced62c3d856cc1654cbddd979dce3f59a7
kcppdevelopers <kcppdevelopers@gmail.com>
2019-02-28 15:33:18 +0530

    update API path, PFWA-11

--- paypal-for-woocommerce.php
+++ paypal-for-woocommerce.php
@@ -51,7 +51,7 @@ if (!defined('PAYPAL_FOR_WOOCOMMERCE_DIR_PATH')) {
     define('PAYPAL_FOR_WOOCOMMERCE_DIR_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ));
 }
 if (!defined('PAYPAL_FOR_WOOCOMMERCE_ISU_URL')) {
-    define('PAYPAL_FOR_WOOCOMMERCE_ISU_URL', 'https://www.angelleye.com/web-services/ifthengive/paypal-isu/');
+    define('PAYPAL_FOR_WOOCOMMERCE_ISU_URL', 'https://www.angelleye.com/web-services/paypal-for-woocommerce/paypal-isu/');
 }
 if (!defined('PAYPAL_FOR_WOOCOMMERCE_PUSH_NOTIFICATION_WEB_URL')) {
     define('PAYPAL_FOR_WOOCOMMERCE_PUSH_NOTIFICATION_WEB_URL', 'https://www.angelleye.com/');
