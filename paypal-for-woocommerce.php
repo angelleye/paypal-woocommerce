98a2d0b74d65f641b3841147fc7d8ba10a841826
kcppdevelopers <kcppdevelopers@gmail.com>
2019-04-01 15:35:26 +0530

    AE Updater plugin link issue, PFW-394

--- paypal-for-woocommerce.php
+++ paypal-for-woocommerce.php
@@ -54,7 +54,7 @@ if (!defined('PAYPAL_FOR_WOOCOMMERCE_PUSH_NOTIFICATION_WEB_URL')) {
     define('PAYPAL_FOR_WOOCOMMERCE_PUSH_NOTIFICATION_WEB_URL', 'https://www.angelleye.com/');
 }
 if (!defined('AEU_ZIP_URL')) {
-    define('AEU_ZIP_URL', 'http://downloads.angelleye.com/ae-updater/angelleye-updater/angelleye-updater.zip');
+    define('AEU_ZIP_URL', 'https://github.com/angelleye/angelleye-updater/archive/master.zip');
 }
 
 /**
