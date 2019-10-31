0aea9ad353f139be0d1506a69880abda34175a08
kcppdevelopers <kcppdevelopers@gmail.com>
2019-10-31 17:17:51 +0530

    Hide all smart button option when Enable Smart Buttons is disable, PFW-517

--- assets/js/angelleye-admin.js
+++ assets/js/angelleye-admin.js
@@ -45,6 +45,9 @@ jQuery(document).ready(function ($) {
             jQuery('woocommerce_paypal_express_enable_google_analytics_click').closest('tr').hide();
             jQuery('#woocommerce_paypal_express_checkout_with_pp_button_type').closest('tr').hide();
             jQuery('.angelleye_smart_button_setting_left').show();
+            jQuery('.in_context_checkout_part').show();
+            jQuery('.in_context_checkout_part').next('p').show();
+            jQuery('.in_context_checkout_part').parents('table').show();
         } else {
             jQuery('.display_smart_button_previews').hide();
             jQuery('.angelleye_button_settings_selector').hide();
@@ -52,7 +55,10 @@ jQuery(document).ready(function ($) {
             jQuery('#woocommerce_paypal_express_checkout_with_pp_button_type').closest('tr').show();
             jQuery('.angelleye_smart_button_setting_left').hide();
             jQuery('woocommerce_paypal_express_enable_google_analytics_click').closest('tr').show();
-
+            jQuery('.in_context_checkout_part').hide();
+            jQuery('.in_context_checkout_part').next('p').hide();
+            jQuery('.in_context_checkout_part').parents('table').hide();
+            
         }
     }).change();
     $("#woocommerce_paypal_express_customer_service_number").attr("maxlength", "16");
