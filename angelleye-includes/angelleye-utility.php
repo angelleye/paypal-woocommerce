f19d5d5b4ea3b36aadfd9e9a94eb7bee48e8373f
Deepak Maurya <deepakmaurya@hotmail.com>
2020-03-30 23:06:15 +0530

    Removed the git conflict message from admin order view, PFW-579

--- angelleye-includes/angelleye-utility.php
+++ angelleye-includes/angelleye-utility.php
@@ -1144,11 +1144,7 @@ class AngellEYE_Utility {
                     </tr>
                     <tr>
                         <td><?php echo __('Total Capture:', 'paypal-for-woocommerce'); ?></td>
-<<<<<<< HEAD
                         <td><?php echo get_woocommerce_currency_symbol($currency_code) . '' . $this->total_DoCapture ?></td>
-=======
-                        <td><?php echo get_woocommerce_currency_symbol( $order->get_currency() ) . '' . $this->total_DoCapture ?></td>
->>>>>>> 7e29fb2fb87417f8e8d23d22c02f2af7379e5836
                     </tr>
                 </tbody>
             </table>
