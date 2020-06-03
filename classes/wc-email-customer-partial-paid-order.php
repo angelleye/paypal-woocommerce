6bc8f7dfa2a42fa553e769df5118f897daa43937
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2020-06-03 17:45:53 +0530

    update template_base path, PFW-636

--- classes/wc-email-customer-partial-paid-order.php
+++ classes/wc-email-customer-partial-paid-order.php
@@ -14,8 +14,8 @@ if (!class_exists('WC_Email_Partially_Paid_Order', false)) :
 
             $this->title = __('Partially Paid order', 'paypal-for-woocommerce');
             $this->description = __('This is an order notification sent to customers containing order details after partially paid.', 'paypal-for-woocommerce');
-            $this->template_html = PAYPAL_FOR_WOOCOMMERCE_DIR_PATH . '/template/emails/angelleye-customer-partial-paid-order.php';
-            $this->template_plain = PAYPAL_FOR_WOOCOMMERCE_DIR_PATH . '/template/emails/plain/angelleye-customer-partial-paid-order.php';
+            $this->template_html = 'angelleye-customer-partial-paid-order.php';
+            $this->template_plain = 'plain/angelleye-customer-partial-paid-order.php';
             $this->placeholders = array(
                 '{order_date}' => '',
                 '{order_number}' => '',
@@ -29,6 +29,8 @@ if (!class_exists('WC_Email_Partially_Paid_Order', false)) :
 
             // Call parent constructor.
             parent::__construct();
+            
+            $this->template_base = PAYPAL_FOR_WOOCOMMERCE_DIR_PATH . '/template/emails';
         }
 
         public function get_default_subject() {
