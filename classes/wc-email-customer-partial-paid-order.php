7b7b062dfbe8f50c5e7ef8b0d4c4726e0cdb57f0
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2020-06-24 16:50:40 +0530

    use setting fields, PFW-643

--- classes/wc-email-customer-partial-paid-order.php
+++ classes/wc-email-customer-partial-paid-order.php
@@ -12,7 +12,7 @@ if (!class_exists('WC_Email_Partially_Paid_Order', false)) :
             $this->id = 'customer_partially_paid_order';
             $this->customer_email = true;
 
-            $this->title = __('Partially Paid order', 'paypal-for-woocommerce');
+            $this->title = __('Partially Paid order - customer', 'paypal-for-woocommerce');
             $this->description = __('This is an order notification sent to customers containing order details after partially paid.', 'paypal-for-woocommerce');
             $this->template_html = 'angelleye-customer-partial-paid-order.php';
             $this->template_plain = 'plain/angelleye-customer-partial-paid-order.php';
@@ -92,6 +92,65 @@ if (!class_exists('WC_Email_Partially_Paid_Order', false)) :
         public function get_default_additional_content() {
             return __('We look forward to fulfilling your order soon.', 'paypal-for-woocommerce');
         }
+        
+        /**
+		 * Initialise settings form fields.
+		 */
+		public function init_form_fields() {
+			/* translators: %s: list of placeholders */
+			$placeholder_text  = sprintf( __( 'Available placeholders: %s', 'woocommerce' ), '<code>' . implode( '</code>, <code>', array_keys( $this->placeholders ) ) . '</code>' );
+			$this->form_fields = array(
+				'enabled'            => array(
+					'title'   => __( 'Enable/Disable', 'woocommerce' ),
+					'type'    => 'checkbox',
+					'label'   => __( 'Enable this email notification', 'woocommerce' ),
+					'default' => 'yes',
+				),
+				'recipient'          => array(
+					'title'       => __( 'Recipient(s)', 'woocommerce' ),
+					'type'        => 'text',
+					/* translators: %s: WP admin email */
+					'description' => sprintf( __( 'Enter recipients (comma separated) for this email. Defaults to %s.', 'woocommerce' ), '<code>' . esc_attr( get_option( 'admin_email' ) ) . '</code>' ),
+					'placeholder' => '',
+					'default'     => '',
+					'desc_tip'    => true,
+				),
+				'subject'            => array(
+					'title'       => __( 'Subject', 'woocommerce' ),
+					'type'        => 'text',
+					'desc_tip'    => true,
+					'description' => $placeholder_text,
+					'placeholder' => $this->get_default_subject(),
+					'default'     => '',
+				),
+				'heading'            => array(
+					'title'       => __( 'Email heading', 'woocommerce' ),
+					'type'        => 'text',
+					'desc_tip'    => true,
+					'description' => $placeholder_text,
+					'placeholder' => $this->get_default_heading(),
+					'default'     => '',
+				),
+				'additional_content' => array(
+					'title'       => __( 'Additional content', 'woocommerce' ),
+					'description' => __( 'Text to appear below the main email content.', 'woocommerce' ) . ' ' . $placeholder_text,
+					'css'         => 'width:400px; height: 75px;',
+					'placeholder' => __( 'N/A', 'woocommerce' ),
+					'type'        => 'textarea',
+					'default'     => $this->get_default_additional_content(),
+					'desc_tip'    => true,
+				),
+				'email_type'         => array(
+					'title'       => __( 'Email type', 'woocommerce' ),
+					'type'        => 'select',
+					'description' => __( 'Choose which format of email to send.', 'woocommerce' ),
+					'default'     => 'html',
+					'class'       => 'email_type wc-enhanced-select',
+					'options'     => $this->get_email_type_options(),
+					'desc_tip'    => true,
+				),
+			);
+		}
 
     }
 
