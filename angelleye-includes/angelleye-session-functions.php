0d2b5786a50a847261fb804d728de761c3c10535
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2020-07-21 18:44:43 +0530

    update session function, PFW-664

--- angelleye-includes/angelleye-session-functions.php
+++ angelleye-includes/angelleye-session-functions.php
@@ -3,32 +3,52 @@
 if (!function_exists('angelleye_set_session')) {
 
     function angelleye_set_session($key, $value) {
-        if (!class_exists('WooCommerce') || WC()->session == null) {
+        if (!class_exists('WooCommerce') || !function_exists('WC')) {
             return false;
         }
-        WC()->session->set($key, $value);
+        if (WC()->session == null) {
+            WC()->initialize_session();
+        }
+        if (WC()->session) {
+            WC()->session->set($key, $value);
+        } else {
+            return false;
+        }
     }
 
 }
 if (!function_exists('angelleye_get_session')) {
 
     function angelleye_get_session($key) {
-        if (!class_exists('WooCommerce') || WC()->session == null) {
+
+        if (!class_exists('WooCommerce') || !function_exists('WC')) {
             return false;
         }
-        $angelleye_session = WC()->session->get($key);
-        return $angelleye_session;
+        if (WC()->session == null) {
+            WC()->initialize_session();
+        }
+        if (WC()->session) {
+            $angelleye_session = WC()->session->get($key);
+            return $angelleye_session;
+        } else {
+            return false;
+        }
     }
 
 }
 if (!function_exists('angelleye_unset_session')) {
 
     function angelleye_unset_session($key) {
-        if (!class_exists('WooCommerce') || WC()->session == null) {
+        if (!class_exists('WooCommerce') || !function_exists('WC')) {
             return false;
         }
-        WC()->session->__unset($key);
-        unset(WC()->session->$key);
+        if (WC()->session == null) {
+            WC()->initialize_session();
+        }
+        if (WC()->session) {
+            WC()->session->__unset($key);
+            unset(WC()->session->$key);
+        }
     }
 
 }
