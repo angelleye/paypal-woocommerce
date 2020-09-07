4958baf83fe23cd93e99113b8ff86393e43d1197
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2020-09-07 14:11:56 +0530

    php notice, PFW-679

--- template/admin.php
+++ template/admin.php
@@ -133,6 +133,7 @@ $gateway = isset($_GET['gateway']) ? wc_clean($_GET['gateway']) : 'paypal_paymen
                     echo '<div class="updated"><p>' . __('Settings were saved successfully.', 'paypal-for-woocommerce') . '</p></div>';
                 }
                 $Force_tls_one_point_two = get_option('Force_tls_one_point_two');
+                $change_proceed_checkout_button_text = get_option('change_proceed_checkout_button_text');
                 ?>
                 <div id="angelleye_paypal_marketing_table">
                 <form method="post">
