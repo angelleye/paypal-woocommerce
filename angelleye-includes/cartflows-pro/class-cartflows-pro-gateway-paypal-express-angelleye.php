52c35d9de4848181f88840b82f67c9c8e68d9f1c
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2021-01-07 15:51:17 +0530

    Troubleshoot Customer Website, PFW-723

--- angelleye-includes/cartflows-pro/class-cartflows-pro-gateway-paypal-express-angelleye.php
+++ angelleye-includes/cartflows-pro/class-cartflows-pro-gateway-paypal-express-angelleye.php
@@ -1215,9 +1215,8 @@ class Cartflows_Pro_Gateway_Paypal_Express_Angelleye extends Cartflows_Pro_Paypa
     public function add_subscription_payment_meta_for_paypal_express($subscription, $order, $offer_product) {
 
         if ('paypal_express' === $order->get_payment_method()) {
-
             $subscription_id = $subscription->get_id();
-
+            update_post_meta($subscription_id, '_payment_tokens_id', $order->get_meta('BILLINGAGREEMENTID', true));
             update_post_meta($subscription_id, 'BILLINGAGREEMENTID', $order->get_meta('BILLINGAGREEMENTID', true));
         }
     }
