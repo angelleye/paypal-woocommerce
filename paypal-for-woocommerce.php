ddb36cdceee1bb4c4be1d778aef429a77e6b42ef
kcppdevelopers <kcppdevelopers@gmail.com>
2021-09-30 15:37:07 +0530

    Update Changelog & Version Number for v2.5.17

--- paypal-for-woocommerce.php
+++ paypal-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name:       PayPal for WooCommerce
  * Plugin URI:        http://www.angelleye.com/product/paypal-for-woocommerce-plugin/
  * Description:       Easily enable PayPal Express Checkout, PayPal Pro, PayPal Advanced, PayPal REST, and PayPal Braintree.  Each option is available separately so you can enable them individually.
- * Version:           2.5.16
+ * Version:           2.5.17
  * Author:            Angell EYE
  * Author URI:        http://www.angelleye.com/
  * License:           GNU General Public License v3.0
@@ -15,7 +15,7 @@
  * Requires at least: 5.5
  * Tested up to: 5.8.1
  * WC requires at least: 3.0.0
- * WC tested up to: 5.6.0
+ * WC tested up to: 5.7.1
  *
  *************
  * Attribution
@@ -39,7 +39,7 @@ if (!defined('PAYPAL_FOR_WOOCOMMERCE_ASSET_URL')) {
     define('PAYPAL_FOR_WOOCOMMERCE_ASSET_URL', plugin_dir_url(__FILE__));
 }
 if (!defined('VERSION_PFW')) {
-    define('VERSION_PFW', '2.5.16');
+    define('VERSION_PFW', '2.5.17');
 }
 if ( ! defined( 'PAYPAL_FOR_WOOCOMMERCE_PLUGIN_FILE' ) ) {
     define( 'PAYPAL_FOR_WOOCOMMERCE_PLUGIN_FILE', __FILE__ );
