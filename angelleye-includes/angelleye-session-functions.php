0b7683e34736fedd06ba74485e980309053239c4
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-10-21 16:05:25 -0400

    Made small changes

--- angelleye-includes/angelleye-session-functions.php
+++ angelleye-includes/angelleye-session-functions.php
@@ -1,12 +1,12 @@
 <?php
 
 if (!function_exists('angelleye_set_session')) {
-
     function angelleye_set_session($key, $value) {
         try {
             if (!class_exists('WooCommerce') || !function_exists('WC')) {
                 return false;
             }
+            
             if (WC()->session == null) {
                 if (version_compare(WC_VERSION, '3.6.3', '>')) {
                     WC()->initialize_session();
@@ -14,24 +14,26 @@ if (!function_exists('angelleye_set_session')) {
                     angelleye_session_init();
                 }
             }
+            
             if (WC()->session) {
                 WC()->session->set($key, $value);
             } else {
                 return false;
             }
+            
         } catch (Exception $ex) {
             
         }
     }
-
 }
-if (!function_exists('angelleye_get_session')) {
 
+if (!function_exists('angelleye_get_session')) {
     function angelleye_get_session($key) {
         try {
             if (!class_exists('WooCommerce') || !function_exists('WC')) {
                 return false;
             }
+            
             if (WC()->session == null) {
                 if (version_compare(WC_VERSION, '3.6.3', '>')) {
                     WC()->initialize_session();
@@ -39,8 +41,10 @@ if (!function_exists('angelleye_get_session')) {
                     angelleye_session_init();
                 }
             }
+            
             if (WC()->session) {
                 $angelleye_session = WC()->session->get($key);
+                
                 return $angelleye_session;
             } else {
                 return false;
@@ -49,16 +53,15 @@ if (!function_exists('angelleye_get_session')) {
             
         }
     }
-
 }
+
 if (!function_exists('angelleye_unset_session')) {
-
     try {
-
         function angelleye_unset_session($key) {
             if (!class_exists('WooCommerce') || !function_exists('WC')) {
                 return false;
             }
+            
             if (WC()->session == null) {
                 if (version_compare(WC_VERSION, '3.6.3', '>')) {
                     WC()->initialize_session();
@@ -66,8 +69,10 @@ if (!function_exists('angelleye_unset_session')) {
                     angelleye_session_init();
                 }
             }
+            
             if (WC()->session) {
                 WC()->session->__unset($key);
+                
                 unset(WC()->session->$key);
             }
         }
@@ -78,12 +83,13 @@ if (!function_exists('angelleye_unset_session')) {
 }
 
 if (!function_exists('angelleye_session_init')) {
-
     function angelleye_session_init() {
         if (is_admin()) {
             return false;
         }
+        
         $old_wc = version_compare(WC_VERSION, '3.0', '<');
+        
         if(!$old_wc) {
             $session_class = apply_filters('woocommerce_session_handler', 'WC_Session_Handler');
             $session = new $session_class();
@@ -92,5 +98,4 @@ if (!function_exists('angelleye_session_init')) {
             return false;
         }
     }
-
 }
