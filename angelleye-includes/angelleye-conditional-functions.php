aa834fcbb4e0a5fa4c9aba8f1a0a8dc95ac906d3
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-10-21 15:49:31 -0400

    Made small changes

--- angelleye-includes/angelleye-conditional-functions.php
+++ angelleye-includes/angelleye-conditional-functions.php
@@ -3,7 +3,6 @@
 defined('ABSPATH') || exit;
 
 if (!function_exists('is_angelleye_ec_review_page')) {
-
     /**
      * is_angelleye_ec_review_page - Returns true when viewing the Express Checkout order review page.
      *
@@ -11,7 +10,8 @@ if (!function_exists('is_angelleye_ec_review_page')) {
      */
     function is_angelleye_ec_review_page() {
         $paypal_express_checkout = angelleye_get_session('paypal_express_checkout');
-        if (isset($paypal_express_checkout['token']) && !empty($paypal_express_checkout['token']) && isset($paypal_express_checkout['payer_id']) && !empty($paypal_express_checkout['payer_id'])) {
+        
+        if (!empty($paypal_express_checkout['token']) && !empty($paypal_express_checkout['payer_id'])) {
             return true;
         } else {
             return false;
@@ -19,22 +19,23 @@ if (!function_exists('is_angelleye_ec_review_page')) {
     }
 
 }
-if (!function_exists('is_angelleye_multi_account_active')) {
 
+if (!function_exists('is_angelleye_multi_account_active')) {
     function is_angelleye_multi_account_active() {
         if (function_exists('run_paypal_for_woocommerce_multi_account_management')) {
             return true;
         }
+        
         return false;
     }
 
 }
 
 if (!function_exists('angelleye_is_us_based_store')) {
-
     function angelleye_is_us_based_store() {
         $base_location = wc_get_base_location();
-        return in_array($base_location['country'], array('US', 'PR', 'GU', 'VI', 'AS', 'MP'), true);
+        
+        return in_array($base_location['country'], ['US', 'PR', 'GU', 'VI', 'AS', 'MP'], true);
     }
 
 }
