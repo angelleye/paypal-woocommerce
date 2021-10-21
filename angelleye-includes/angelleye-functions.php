9e74375cdcf08d90e9c1108f9207707c4bdc0cf2
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-10-21 15:53:55 -0400

    Made small changes

--- angelleye-includes/angelleye-functions.php
+++ angelleye-includes/angelleye-functions.php
@@ -7,12 +7,12 @@
  * Queue updates for the Angell EYE Updater
  */
 if (!function_exists('angelleye_queue_update')) {
-
     function angelleye_queue_update($file, $file_id, $product_id) {
         global $angelleye_queued_updates;
 
-        if (!isset($angelleye_queued_updates))
-            $angelleye_queued_updates = array();
+        if (!isset($angelleye_queued_updates)) {
+            $angelleye_queued_updates = [];
+        }
 
         $plugin = new stdClass();
         $plugin->file = $file;
@@ -24,27 +24,23 @@ if (!function_exists('angelleye_queue_update')) {
 
 }
 
-
 /**
  * Load installer for the AngellEYE Updater.
  * @return $api Object
  */
 if (!class_exists('AngellEYE_Updater') && !function_exists('angell_updater_install')) {
-
     function angell_updater_install($api, $action, $args) {
         $download_url = AEU_ZIP_URL;
 
-        if ('plugin_information' != $action ||
-                false !== $api ||
-                !isset($args->slug) ||
-                'angelleye-updater' != $args->slug
-        )
+        if ('plugin_information' != $action || false !== $api || !isset($args->slug) || 'angelleye-updater' != $args->slug) {
             return $api;
+        }
 
-        $api = new stdClass();
+        $api = new \stdClass();
         $api->name = 'AngellEYE Updater';
         $api->version = '';
         $api->download_link = esc_url($download_url);
+        
         return $api;
     }
 
@@ -55,37 +51,47 @@ if (!class_exists('AngellEYE_Updater') && !function_exists('angell_updater_insta
  * AngellEYE Installation Prompts
  */
 if (!class_exists('AngellEYE_Updater') && !function_exists('angell_updater_notice')) {
-
     /**
      * Display a notice if the "AngellEYE Updater" plugin hasn't been installed.
      * @return void
      */
     function angell_updater_notice() {
         $active_plugins = apply_filters('active_plugins', get_option('active_plugins'));
-        if (in_array('angelleye-updater/angelleye-updater.php', $active_plugins))
+        
+        if (in_array('angelleye-updater/angelleye-updater.php', $active_plugins)) {
             return;
+        }
 
         $slug = 'angelleye-updater';
+        
         $install_url = wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=' . $slug), 'install-plugin_' . $slug);
+        
         $activate_url = 'plugins.php?action=activate&plugin=' . urlencode('angelleye-updater/angelleye-updater.php') . '&plugin_status=all&paged=1&s&_wpnonce=' . urlencode(wp_create_nonce('activate-plugin_angelleye-updater/angelleye-updater.php'));
 
         $message = '<a href="' . esc_url($install_url) . '">Install the Angell EYE Updater plugin</a> to get updates for your Angell EYE plugins.';
+        
         $is_downloaded = false;
+        
         $plugins = array_keys(get_plugins());
+        
         foreach ($plugins as $plugin) {
             if (strpos($plugin, 'angelleye-updater.php') !== false) {
                 $is_downloaded = true;
+                
                 $message = '<a href="' . esc_url(admin_url($activate_url)) . '"> Activate the Angell EYE Updater plugin</a> to get updates for your Angell EYE plugins.';
             }
         }
+        
         echo '<div id="angelleye-updater-notice" class="updated notice updater-dismissible"><p>' . $message . '</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>' . "\n";
     }
     
     function angelleye_updater_dismissible_admin_notice() {
-        set_transient( 'angelleye_updater_notice_hide', 'yes', MONTH_IN_SECONDS );
+        set_transient('angelleye_updater_notice_hide', 'yes', MONTH_IN_SECONDS);
     }
-    if ( false === ( $angelleye_updater_notice_hide = get_transient( 'angelleye_updater_notice_hide' ) ) ) {
+    
+    if (false === ($angelleye_updater_notice_hide = get_transient('angelleye_updater_notice_hide'))) {
         add_action('admin_notices', 'angell_updater_notice');
     }
-    add_action( 'wp_ajax_angelleye_updater_dismissible_admin_notice', 'angelleye_updater_dismissible_admin_notice' );
+    
+    add_action('wp_ajax_angelleye_updater_dismissible_admin_notice', 'angelleye_updater_dismissible_admin_notice');
 }
