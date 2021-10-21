90f7160fe6bca128464aadcc6926171b20766ba0
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-10-20 22:54:30 -0400

    Made various changes

--- angelleye-includes/express-checkout/class-wc-gateway-paypal-express-response-angelleye.php
+++ angelleye-includes/express-checkout/class-wc-gateway-paypal-express-response-angelleye.php
@@ -8,34 +8,43 @@ class WC_Gateway_PayPal_Express_Response_AngellEYE {
 
     public function ec_get_shipping_details($response) {
         try {
-            require_once( PAYPAL_FOR_WOOCOMMERCE_PLUGIN_DIR . '/classes/lib/NameParser.php' );
-            $parser = new FullNameParser();
-            $details = array();
+            require_once(PAYPAL_FOR_WOOCOMMERCE_PLUGIN_DIR . '/classes/lib/NameParser.php');
+            
+            $parser = new \FullNameParser();
+            
+            $details = [];
+            
             if (isset($response['SHIPTONAME'])) {
                 $split_name = $parser->split_full_name($response['SHIPTONAME']);
+                
                 $shipping_first_name = $split_name['fname'];
                 $shipping_last_name = $split_name['lname'];
+                
                 $company = !empty($response['BUSINESS']) ? str_replace("\\","", $response['BUSINESS']) : '';
+                
                 $phone = '';
+                
                 if( !empty($response['SHIPTOPHONENUM']) ) {
                     $phone = $response['SHIPTOPHONENUM'];
                 } elseif( !empty($response['PHONENUM']) ) {
                     $phone = $response['PHONENUM'];
                 }
-                $details = array(
-                    'first_name' => isset($shipping_first_name) ? $shipping_first_name : $response['FIRSTNAME'],
-                    'last_name' => isset($shipping_last_name) ? $shipping_last_name : $response['LASTNAME'],
-                    'company' => $company,
-                    'email' => isset($response['EMAIL']) ? $response['EMAIL'] : '',
-                    'phone' => $phone,
-                    'address_1' => isset($response['SHIPTOSTREET']) ? $response['SHIPTOSTREET'] : '',
-                    'address_2' => isset($response['SHIPTOSTREET2']) ? $response['SHIPTOSTREET2'] : '',
-                    'city' => isset($response['SHIPTOCITY']) ? $response['SHIPTOCITY'] : '',
-                    'postcode' => isset($response['SHIPTOZIP']) ? $response['SHIPTOZIP'] : '',
-                    'country' => isset($response['SHIPTOCOUNTRYCODE']) ? $response['SHIPTOCOUNTRYCODE'] : '',
-                    'state' => (isset($response['SHIPTOCOUNTRYCODE']) && isset($response['SHIPTOSTATE'])) ? $this->ec_get_state_code($response['SHIPTOCOUNTRYCODE'], $response['SHIPTOSTATE']) : ''
-                );
+                
+                $details = [
+                    'first_name'    => isset($shipping_first_name) ? $shipping_first_name : $response['FIRSTNAME'],
+                    'last_name'     => isset($shipping_last_name) ? $shipping_last_name : $response['LASTNAME'],
+                    'company'       => $company,
+                    'email'         => isset($response['EMAIL']) ? $response['EMAIL'] : '',
+                    'phone'         => $phone,
+                    'address_1'     => isset($response['SHIPTOSTREET']) ? $response['SHIPTOSTREET'] : '',
+                    'address_2'     => isset($response['SHIPTOSTREET2']) ? $response['SHIPTOSTREET2'] : '',
+                    'city'          => isset($response['SHIPTOCITY']) ? $response['SHIPTOCITY'] : '',
+                    'postcode'      => isset($response['SHIPTOZIP']) ? $response['SHIPTOZIP'] : '',
+                    'country'       => isset($response['SHIPTOCOUNTRYCODE']) ? $response['SHIPTOCOUNTRYCODE'] : '',
+                    'state'         => (isset($response['SHIPTOCOUNTRYCODE']) && isset($response['SHIPTOSTATE'])) ? $this->ec_get_state_code($response['SHIPTOCOUNTRYCODE'], $response['SHIPTOSTATE']) : ''
+                ];
             }
+            
             return $details;
         } catch (Exception $ex) {
 
@@ -45,22 +54,26 @@ class WC_Gateway_PayPal_Express_Response_AngellEYE {
     public function ec_get_state_code($country, $state) {
         try {
             $valid_states = WC()->countries->get_states($country);
+            
             if (!empty($valid_states) && is_array($valid_states)) {
                 $valid_state_values = array_flip(array_map('strtolower', $valid_states));
+                
                 if (isset($valid_state_values[strtolower($state)])) {
                     $state_value = $valid_state_values[strtolower($state)];
+                    
                     return $state_value;
                 }
             } else {
                 return $state;
             }
-            if (!empty($valid_states) && is_array($valid_states) && sizeof($valid_states) > 0) {
+            if (!empty($valid_states) && is_array($valid_states)) {
                 if (!in_array(strtoupper($state), array_keys($valid_states))) {
                     return false;
                 } else {
                     return strtoupper($state);
                 }
             }
+            
             return $state;
         } catch (Exception $ex) {
 
