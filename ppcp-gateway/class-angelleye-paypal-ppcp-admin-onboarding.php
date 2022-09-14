1a065fa6f066f00f481efa419081d8941e38f085
kcppdevelopers <kcppdevelopers@gmail.com>
2022-09-14 17:52:47 +0530

    User Onboarding, PFW-1074

--- ppcp-gateway/class-angelleye-paypal-ppcp-admin-onboarding.php
+++ ppcp-gateway/class-angelleye-paypal-ppcp-admin-onboarding.php
@@ -115,7 +115,60 @@ class AngellEYE_PayPal_PPCP_Admin_Onboarding {
         }
     }
 
+    public function angelleye_get_signup_link($testmode = 'yes', $page) {
+        try {
+            $seller_onboarding_result = $this->seller_onboarding->angelleye_generate_signup_link($testmode, $page);
+            if (isset($seller_onboarding_result['links'])) {
+                foreach ($seller_onboarding_result['links'] as $link) {
+                    if (isset($link['rel']) && 'action_url' === $link['rel']) {
+                        return isset($link['href']) ? $link['href'] : false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        } catch (Exception $ex) {
+            
+        }
+    }
+
+    public function angelleye_ppcp_admin_notices() {
+        $is_saller_onboarding_done = false;
+        $is_saller_onboarding_failed = false;
+        if (false !== get_transient('angelleye_ppcp_sandbox_seller_onboarding_process_done')) {
+            $is_saller_onboarding_done = true;
+            delete_transient('angelleye_ppcp_sandbox_seller_onboarding_process_done');
+        } elseif (false !== get_transient('angelleye_ppcp_live_seller_onboarding_process_done')) {
+            $is_saller_onboarding_done = true;
+            delete_transient('angelleye_ppcp_live_seller_onboarding_process_done');
+        }
+        if ($is_saller_onboarding_done) {
+            echo '<div class="notice notice-success angelleye-notice is-dismissible" id="ppcp_success_notice_onboarding" style="display:none;">'
+            . '<div class="angelleye-notice-logo-original">'
+            . '<div class="ppcp_success_logo"><img src="' . PAYPAL_FOR_WOOCOMMERCE_ASSET_URL . 'assets/images/ppcp_check_mark.png" width="65" height="65"></div>'
+            . '</div>'
+            . '<div class="angelleye-notice-message">'
+            . '<h3>PayPal onboarding process successfully completed.</h3>'
+            . '</div>'
+            . '</div>';
+        } else {
+            if (false !== get_transient('angelleye_ppcp_sandbox_seller_onboarding_process_failed')) {
+                $is_saller_onboarding_failed = true;
+                delete_transient('angelleye_ppcp_sandbox_seller_onboarding_process_failed');
+            } elseif (false !== get_transient('angelleye_ppcp_live_seller_onboarding_process_failed')) {
+                $is_saller_onboarding_failed = true;
+                delete_transient('angelleye_ppcp_live_seller_onboarding_process_failed');
+            }
+            if ($is_saller_onboarding_failed) {
+                echo '<div class="notice notice-error is-dismissible">'
+                . '<p>We could not properly connect to PayPal. Please reload the page to continue.</p>'
+                . '</div>';
+            }
+        }
+    }
+
     public function view() {
+        //$this->angelleye_ppcp_admin_notices();
         ?>    
         <div id="angelleye_paypal_marketing_table">
             <?php if ($this->on_board_status === 'NOT_CONNECTED') { ?>
@@ -124,10 +177,36 @@ class AngellEYE_PayPal_PPCP_Admin_Onboarding {
                         <span class="ppcp_onbard_icon"><img class="image" src="<?php echo PAYPAL_FOR_WOOCOMMERCE_ASSET_URL . 'ppcp-gateway/images/admin/ppcp_admin_onbard_icon.png'; ?>"></span>
                         <br><br><br>
                         <div class="paypal_woocommerce_product_onboard_content">
-                            <p><?php echo __('Welcome to the easiest one-stop solution for accepting PayPal, Debit and Credit <br>Cards, with a lower per-transaction cost for cards than other gateways!', ''); ?></p>
-                            <a href="https://wplaunchify.com/newsletter/" class="wplk-button" target="_blank"><?php echo __('Start Now', ''); ?></a>
-                            <p><?php echo __('Buyers may pay with Debit/Credit (no PayPal account required), <br>and your fee will be only 2.69% + 49¢!', ''); ?></p>
-                            <p><?php echo __('Buyers may also choose to pay with <br>PayPal Checkout, Pay Later, Venmo, and more!', ''); ?></p>    
+                            <p><?php echo __('Welcome to the easiest one-stop solution for accepting PayPal, Debit and Credit <br>Cards, with a lower per-transaction cost for cards than other gateways!', 'paypal-for-woocommerce'); ?></p>
+                            <?php
+                            $testmode = $this->sandbox ? 'yes' : 'no';
+                            $signup_link = $this->angelleye_get_signup_link($testmode, 'admin_settings_onboarding');
+                            if ($signup_link) {
+                                $args = array(
+                                    'displayMode' => 'minibrowser',
+                                );
+                                $url = add_query_arg($args, $signup_link);
+                                ?>
+                                <a target="_blank" class="wplk-button" id="<?php echo esc_attr('wplk-button'); ?>" data-paypal-onboard-complete="onboardingCallback" href="<?php echo esc_url($url); ?>" data-paypal-button="true"><?php echo __('Start Now', 'paypal-for-woocommerce'); ?></a>    
+                                <?php
+                                $script_url = 'https://www.paypal.com/webapps/merchantboarding/js/lib/lightbox/partner.js';
+                                ?>
+                                <script type="text/javascript">
+                                    document.querySelectorAll('[data-paypal-onboard-complete=onboardingCallback]').forEach((element) => {
+                                        element.addEventListener('click', (e) => {
+                                            if ('undefined' === typeof PAYPAL) {
+                                                e.preventDefault();
+                                                alert('PayPal');
+                                            }
+                                        });
+                                    });</script>
+                                <script id="paypal-js" src="<?php echo esc_url($script_url); ?>"></script> <?php
+                            } else {
+                                echo __('We could not properly connect to PayPal', '');
+                            }
+                            ?>  
+                            <p><?php echo __('Buyers may pay with Debit/Credit (no PayPal account required), <br>and your fee will be only 2.69% + 49¢!', 'paypal-for-woocommerce'); ?></p>
+                            <p><?php echo __('Buyers may also choose to pay with <br>PayPal Checkout, Pay Later, Venmo, and more!', 'paypal-for-woocommerce'); ?></p>    
                         </div>
                     </div>
                 </div>
@@ -139,13 +218,13 @@ class AngellEYE_PayPal_PPCP_Admin_Onboarding {
                         <div class="paypal_woocommerce_product_onboard_content">
                             <br>
                             <span><img class="green_checkmark" src="<?php echo PAYPAL_FOR_WOOCOMMERCE_ASSET_URL . 'ppcp-gateway/images/admin/green_checkmark.png'; ?>"></span>
-                            <p><?php echo __('You’re currently setup and enjoying the benefits of <br>WooCommerce Complete Payments.', ''); ?></p>
-                            <p><?php echo __('However, we need additional verification to approve you for the reduced <br>rate of 2.69% on debit/credit cards.', ''); ?></p>
-                            <p><?php echo __('To apply for a reduced rate, modify your setup, <br>or learn more about additional options, please use the buttons below.', ''); ?></p>    
+                            <p><?php echo __('You’re currently setup and enjoying the benefits of <br>WooCommerce Complete Payments.', 'paypal-for-woocommerce'); ?></p>
+                            <p><?php echo __('However, we need additional verification to approve you for the reduced <br>rate of 2.69% on debit/credit cards.', 'paypal-for-woocommerce'); ?></p>
+                            <p><?php echo __('To apply for a reduced rate, modify your setup, <br>or learn more about additional options, please use the buttons below.', 'paypal-for-woocommerce'); ?></p>    
                             <br>
-                            <a href="https://wplaunchify.com/newsletter/" class="green-button" target="_blank"><?php echo __('Apply for Cheaper Fees!', ''); ?></a>
-                            <a href="https://wplaunchify.com/newsletter/" class="wplk-button" target="_blank"><?php echo __('Modify Setup', ''); ?></a>
-                            <a href="https://wplaunchify.com/newsletter/" class="slate_gray" target="_blank"><?php echo __('Learn More', ''); ?></a>
+                            <a href="https://www.angelleye.com" class="green-button" target="_blank"><?php echo __('Apply for Cheaper Fees!', 'paypal-for-woocommerce'); ?></a>
+                            <a href="<?php echo admin_url('admin.php?page=wc-settings&tab=checkout&section=angelleye_ppcp'); ?>" class="wplk-button" target="_blank"><?php echo __('Modify Setup', 'paypal-for-woocommerce'); ?></a>
+                            <a href="https://www.angelleye.com/paypal-complete-payments-setup-guide/" class="slate_gray" target="_blank"><?php echo __('Learn More', 'paypal-for-woocommerce'); ?></a>
                             <br><br>
                         </div>
                     </div>
@@ -158,12 +237,12 @@ class AngellEYE_PayPal_PPCP_Admin_Onboarding {
                         <div class="paypal_woocommerce_product_onboard_content">
                             <br>
                             <span><img class="green_checkmark" src="<?php echo PAYPAL_FOR_WOOCOMMERCE_ASSET_URL . 'ppcp-gateway/images/admin/green_checkmark.png'; ?>"></span>
-                            <p><?php echo __('You’re currently setup and enjoying the benefits of <br> WooCommerce Complete Payments.', ''); ?></p>
-                            <p><?php echo __('This includes a reduced rate for debit / credit cards of only 2.69% + 49¢!', ''); ?></p>
-                            <p><?php echo __('To modify your setup or learn more about additional options, <br> please use the buttons below.', ''); ?></p>   
+                            <p><?php echo __('You’re currently setup and enjoying the benefits of <br> WooCommerce Complete Payments.', 'paypal-for-woocommerce'); ?></p>
+                            <p><?php echo __('This includes a reduced rate for debit / credit cards of only 2.69% + 49¢!', 'paypal-for-woocommerce'); ?></p>
+                            <p><?php echo __('To modify your setup or learn more about additional options, <br> please use the buttons below.', 'paypal-for-woocommerce'); ?></p>   
                             <br>
-                            <a href="https://wplaunchify.com/newsletter/" class="wplk-button" target="_blank"><?php echo __('Modify Setup', ''); ?></a>
-                            <a href="https://wplaunchify.com/newsletter/" class="slate_gray" target="_blank"><?php echo __('Learn More', ''); ?></a>
+                            <a href="<?php echo admin_url('admin.php?page=wc-settings&tab=checkout&section=angelleye_ppcp'); ?>" class="wplk-button" target="_blank"><?php echo __('Modify Setup', 'paypal-for-woocommerce'); ?></a>
+                            <a href="https://www.angelleye.com/paypal-complete-payments-setup-guide/" class="slate_gray" target="_blank"><?php echo __('Learn More', 'paypal-for-woocommerce'); ?></a>
                             <br><br>
                         </div>
                     </div>
@@ -176,7 +255,7 @@ class AngellEYE_PayPal_PPCP_Admin_Onboarding {
                         <div class="paypal_woocommerce_product_onboard_content">
                             <br>
                             <span><img class="green_checkmark" src="<?php echo PAYPAL_FOR_WOOCOMMERCE_ASSET_URL . 'ppcp-gateway/images/admin/green_checkmark.png'; ?>"></span>
-                            <p><?php echo __('First Party WooCommerce Complete Payments Used.', ''); ?></p>
+                            <p><?php echo __('First Party WooCommerce Complete Payments Used.', 'paypal-for-woocommerce'); ?></p>
                         </div>
                     </div>
                 </div>
