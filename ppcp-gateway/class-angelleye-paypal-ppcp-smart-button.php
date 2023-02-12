5f9dd127e28ea9cdd64819a17b94482bc5048c68
kcppdevelopers <kcppdevelopers@gmail.com>
2023-02-12 07:04:43 +0530

    update

--- ppcp-gateway/class-angelleye-paypal-ppcp-smart-button.php
+++ ppcp-gateway/class-angelleye-paypal-ppcp-smart-button.php
@@ -807,9 +807,13 @@ class AngellEYE_PayPal_PPCP_Smart_Button {
             $client_token = '';
             $user_id_token = '';
             if (!isset($_GET['paypal_order_id'])) {
-                if ($this->enable_tokenized_payments || ((is_checkout() || is_checkout_pay_page()) && $this->advanced_card_payments)) {
-                    $this->client_token = $this->payment_request->angelleye_ppcp_get_generate_token();
-                    $client_token = "data-client-token='{$this->client_token}'";
+                if ((is_checkout() || is_checkout_pay_page()) && $this->advanced_card_payments) {
+                    // $this->client_token = $this->payment_request->angelleye_ppcp_get_generate_token();
+                    // $client_token = "data-client-token='{$this->client_token}'";
+                }
+                if ($this->enable_tokenized_payments) {
+                    $this->id_token = $this->angelleye_ppcp_get_generate_id_token();
+                    $user_id_token = " data-user-id-token='{$this->id_token}'";
                 }
             }
             $tag = str_replace(' src=', ' ' . $client_token . $user_id_token . ' data-namespace="angelleye_paypal_sdk" src=', $tag);
