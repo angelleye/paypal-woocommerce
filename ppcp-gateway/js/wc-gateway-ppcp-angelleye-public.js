5a8fb089fcfc4d8ceddfef68bbc7046c75e62c90
Jignesh <jignesh@securemysanity.com>
2023-04-07 16:16:49 +0530

    resolved JS conflict, PFW-779

--- ppcp-gateway/js/wc-gateway-ppcp-angelleye-public.js
+++ ppcp-gateway/js/wc-gateway-ppcp-angelleye-public.js
@@ -27,22 +27,9 @@
             if (is_hosted_field_eligible() === false) {
                 $('.payment_method_angelleye_ppcp_cc').hide();
             }
-            if (angelleye_ppcp_manager.advanced_card_payments === 'yes') {
-                if (angelleye_ppcp_manager.enable_separate_payment_method === 'no') {
-                    if (is_hosted_field_eligible() === true) {
-                        $('#place_order').show();
-                    } else if (is_angelleye_ppcp_selected() === true && angelleye_ppcp_manager.is_checkout_disable_smart_button === 'yes') {
-                        $('#place_order').show();
-                    } else {
-                        $('#place_order').hide();
-                    }
-                } else if (angelleye_ppcp_manager.enable_separate_payment_method === 'yes') {
-                    if (is_angelleye_ppcp_selected() === true && angelleye_ppcp_manager.is_checkout_disable_smart_button === 'no') {
-                        $('#place_order').hide();
-                    } else {
-                        $('#place_order').show();
-                    }
-                }
+             if (is_angelleye_ppcp_selected() === true && angelleye_ppcp_manager.is_checkout_disable_smart_button === 'no') {
+                $('#place_order').hide();
+                $('#angelleye_ppcp_checkout').show();
             } else {
                 $('#angelleye_ppcp_checkout').hide();
                 $('#place_order').show();
