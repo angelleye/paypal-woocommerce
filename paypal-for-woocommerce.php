a568c2a42aa5795ee4319f3f5c7c82cbab8c21e1
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2023-07-14 13:05:22 +0530

    High Performance Order Storage Compatibility, PFW-1483

--- paypal-for-woocommerce.php
+++ paypal-for-woocommerce.php
@@ -1635,3 +1635,9 @@ if(!class_exists('AngellEYE_Gateway_Paypal')){
     }
 }
 new AngellEYE_Gateway_Paypal();
+
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
