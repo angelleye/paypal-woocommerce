ae4293a62061076020b6524faf7502c6a715b26c
kcppdevelopers <kcppdevelopers@users.noreply.github.com>
2023-08-21 18:28:05 +0530

    PFW-1560, Update

--- ppcp-gateway/class-wc-gateway-cc-angelleye.php
+++ ppcp-gateway/class-wc-gateway-cc-angelleye.php
@@ -19,6 +19,8 @@ class WC_Gateway_CC_AngellEYE extends WC_Payment_Gateway_CC {
     public $client_id;
     public $secret_id;
     public $enable_tokenized_payments;
+    public $paymentaction;
+    public $checkout_disable_smart_button;
 
     public function __construct() {
         try {
@@ -39,6 +41,7 @@ class WC_Gateway_CC_AngellEYE extends WC_Payment_Gateway_CC {
             $this->sandbox_secret_id = $this->setting_obj->get('sandbox_api_secret', '');
             $this->live_client_id = $this->setting_obj->get('api_client_id', '');
             $this->live_secret_id = $this->setting_obj->get('api_secret', '');
+            $this->paymentaction = $this->get_option('paymentaction', 'capture');
             $this->advanced_card_payments = 'yes' === $this->setting_obj->get('enable_advanced_card_payments', 'no');
             if ($this->dcc_applies->for_country_currency() === false) {
                 $this->advanced_card_payments = false;
