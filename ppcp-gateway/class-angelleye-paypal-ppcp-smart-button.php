1b9a97dc67ed851f474bfc2f09defa8c4c335ecc
Deepak Maurya <deepakmaurya@hotmail.com>
2026-03-14 18:01:53 +0530

    Remove buyer country from live SDK envs, enable only for sandbox

--- ppcp-gateway/class-angelleye-paypal-ppcp-smart-button.php
+++ ppcp-gateway/class-angelleye-paypal-ppcp-smart-button.php
@@ -560,8 +560,10 @@ class AngellEYE_PayPal_PPCP_Smart_Button {
                 array_push($enable_funding, 'ideal');
             }
 
-            if (WC()->customer && WC()->customer->get_billing_country() && 2 === strlen(WC()->customer->get_billing_country())) {
-                $smart_js_arg['buyer-country'] = WC()->customer->get_billing_country();
+            if ($this->is_sandbox){
+                if (WC()->customer && WC()->customer->get_billing_country() && 2 === strlen(WC()->customer->get_billing_country())) {
+                    $smart_js_arg['buyer-country'] = WC()->customer->get_billing_country();
+                }
             }
             $product_cart_amounts = $this->payment_request->ae_get_updated_checkout_payment_data();
 
